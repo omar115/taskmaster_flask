47c86cabc1d09f58efa3af89ec454db098e969e6
omar115 <omarhasan115@gmail.com>
2021-04-18 15:04:44 +0600

    update featuer added

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import re
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
@@ -35,6 +36,17 @@ def index():
         # pass
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There is an error while deleting data'
 
 if __name__ == '__main__':
     app.run(debug=True)
